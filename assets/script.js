c8b572a311287d48f83cdaa076a1e6143eabec72
Jeramy Sena <jeramy_sena@hotmail.com>
2021-10-30 14:07:00 -0500

    add input search data

--- assets/script.js
+++ assets/script.js
@@ -1,45 +1,44 @@
 // Add CSS to HTML code
-var citySearch = document.getElementById("#city-search");
-var searchField = document.getElementById("#search-city");
-var searchHistory = document.getElementById("#search-history");
-var searchResults = document.getElementById("#search-results");
-var weatherForm = document.getElementById("#weather-form");
-var weatherData = document.getElementById("#weather-data");
-var fiveDayForecast = document.getElementById("#five-day-forecast");
-var forecastBoxes = document.getElementById("#forecast-boxes");
+var searchInputEl = document.getElementById("search-input");
+var searchButtonEl = document.getElementById("search-button");
+var searchHistoryEl = document.getElementById("search-history");
+var searchResultsEl = document.getElementById("search-results");
+var weatherFormEl = document.getElementById("weather-form");
+var weatherDataEl = document.getElementById("weather-data");
+var fiveDayForecastEl = document.getElementById("five-day-forecast");
+var forecastBoxesEl = document.getElementById("forecast-boxes");
 
-// Add Search for City Input Data
-var searchCity = function(cityWeather) {
-    var weather = cityWeather;
-    var weatherAPI = "http://api.openweathermap.org/data/2.5/weather?access_key=3378d404aa373dff507fddd9b837d09e&query=" + weather + "&limit=1";
-    
-    fetch(weatherAPI)
-    .then(function(response) {
-        if (response.ok) {
-            response.json().then(function(location) {
-                console.log(location, city);
-                cityWeather(location, cityWeather);
-            })
-        } else {
-            alert("Error: City not found.");
-        }
-    })
-}
+// Search for City Input Data
+var apiKey = "&appid=3378d404aa373dff507fddd9b837d09e";
+var apiGeoURL = "http://api.openweathermap.org/geo/1.0/direct?q=";
+
+searchButtonEl.addEventListener("click", function (event) {
+  event.preventDefault();
+  console.log(searchInputEl.value);
+  var searchURL = apiGeoURL + searchInputEl.value + apiKey;
+  console.log(searchURL);
+  fetch(searchURL).then(function (response) {
+    console.log(response);
+    response.json().then(function (data) {
+      console.log(data);
+      var cityGeoCoord = data[0];
+      console.log(cityGeoCoord);
+      var longitide = cityGeoCoord.lon;
+      var latitude = cityGeoCoord.lat;
+      console.log(longitide);
+      console.log(latitude);
+    });
+  });
+});
+
+// Search for City Input Data
 
 // Retrieve Weather Data for Searched City
-var retrieveWeather = function (cityWeather) {
-    var weatherAPI = "http://api.openweathermap.org/data/2.5/weather?access_key=3378d404aa373dff507fddd9b837d09e&query=" + weather + "&limit=1";
-    fetch(weatherAPI)
-        .then(function(response) {
-            if (response.ok) {
-                response.json().then(function(data) {
-                    console.log(data);
-                    displayWeather(data);
-                });
-            } else {
-                alert("Error: City not found.");
-            }
-    })
-}
 
+// Save City Searches to Local Storage
 
+// Retrieve Weather Data from Local Storage Input
+
+// Show 5-Day Forecast
+
+// Event Listeners for City Search and Local Storage Cities
